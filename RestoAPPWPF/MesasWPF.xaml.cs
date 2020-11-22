0906bc6e089c10da198ed9921ad66ccf61379c5e
benjamin <benjaignacioiv@gmail.com>
2020-11-22 16:24:05 -0300

    VALIDACION SOLO NUMEROS EN MESAS LISTA

--- RestoAPPWPF/MesasWPF.xaml.cs
+++ RestoAPPWPF/MesasWPF.xaml.cs
@@ -331,11 +331,40 @@ namespace RestoAPPWPF
             this.WindowState = WindowState.Minimized;
         }
 
+        private void grAgregar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9){
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
 
+        private void txtNroMesaMod_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
 
-
-
-
+        private void txtCantSillasMod_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
         // -- METODOS -- //
 
     }
